521ae0aa3ca128202c72602bef3bc5b64f331687
Muhammad Hanzala <hanzalaarohan@gmail.com>
2019-09-17 03:01:12 +0500

    first Rust game completed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+fn main()
+{
+    println!("Guess the number");
+    let secret_number =rand::thread_rng().gen_range(1,101);
+    loop
+    {
+        println!("Enter your guess");    
+    let mut guess =String::new();
+    io::stdin().read_line(&mut guess).expect("failed to read");
+    let guess: u32=match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => continue,
+
+    };
+    match guess.cmp(&secret_number)
+    {
+        Ordering::Less => println!("less"),
+        Ordering::Greater => println!("greater"),
+        Ordering::Equal =>
+        {
+            println!("Great you won");
+            break;
+        }
+    }
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
